3d688137cbd2dcc78e41917d874d1f51bb5a4c8b
julianagonzalezastudillo <68652754+julianagonzalezastudillo@users.noreply.github.com>
2023-04-12 14:09:44 +0200

    unify parameters in params.json

--- classif_csp.py
+++ classif_csp.py
@@ -1,3 +1,4 @@
+import os
 from sklearn.pipeline import make_pipeline
 from sklearn.svm import SVC
 
@@ -12,23 +13,29 @@ from moabb.datasets import (BNCI2014001,
                             Cho2017,
                             Schirrmeister2017,
                             Weibo2014)
-
 from moabb.paradigms import LeftRightImagery
 
 from netfeat_pipeline import WithinSessionEvaluation_netfeat
+import json
+import pathlib
 
 
-# for rh vs lh
+path = pathlib.Path().resolve()
+jsonfile = "params.json"
+if jsonfile in os.listdir(path):
+    jsonfullpath = os.path.join(path, jsonfile)
+    with open(jsonfullpath) as jsonfile:
+        parameterDict = json.load(jsonfile)
+fmin = parameterDict['fmin']
+fmax = parameterDict['fmax']
+csp_n_components = parameterDict['csp_n_components']
+csp_component_order = parameterDict['csp_component_order']
+
 # datasets = [BNCI2014001(), Cho2017(), Lee2019_MI(), Schirrmeister2017(), Weibo2014(), Zhou2016()]
 # montages = ['standard_1020', 'standard_1020', 'standard_1005', 'standard_1005', 'standard_1020', 'standard_1020']
 datasets = [BNCI2014001()]
 montages = ['standard_1020']
 
-fmin = 8
-fmax = 35
-csp_component_order = 'mutual_info'
-# csp_component_order = 'alternate'
-csp_n_components = 8
 
 pipeline = {"CSP+PS+SVM": make_pipeline(
     CSP(n_components=csp_n_components, reg=None, transform_into='average_power', component_order=csp_component_order),
