91ee9e212da84abe83cc09bab43b0855f461bf22
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2023-08-09 15:29:32 +0200

    import bcolors from tools.py

--- networktools/net_topo.py
+++ networktools/net_topo.py
@@ -6,18 +6,7 @@ import sys
 import numpy as np
 import re
 import mne
-
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+from tools import bcolors
 
 
 def positions_matrix(montage_name, ch_names):
