f0e8e8113755dc171298d715030ed216eba6eb58
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2023-09-21 18:04:59 +0200

    save info file, with subject information to fasten posterior analysis

--- moabb_settings.py
+++ moabb_settings.py
@@ -1,5 +1,9 @@
 """ This file contains auxiliary functions """
 import save_features
+from pathlib import Path
+import pickle
+import gzip
+import os
 
 
 def add_attributes(obj, **kwargs):
@@ -25,7 +29,7 @@ def save_global(params):
     }
     save_features.df_select = save_features.df_select.append(selec, ignore_index=True)
 
-    if params.dataset.sub == params.dataset.subject_list[-1]\
+    if params.dataset.sub == params.dataset.subject_list[-1] \
             and save_features.n_cv == params.cv_splits \
             and params.session == params.sessions_name[-1]:
         df_save = save_features.df_select[(save_features.df_select["dataset"] == params.dataset.code) &
@@ -34,3 +38,28 @@ def save_global(params):
                        .format(params.dataset.code, params.pipeline), index=False)
     save_features.n_cv = save_features.n_cv + 1
 
+
+def create_info_file(dataset, subject, paradigm):
+    """
+    Create info file with subject information in order to fasten when loading information.
+    """
+    info_path = Path(os.path.join(os.getcwd(), 'results', 'info'))
+    if not os.path.exists(info_path):
+        os.makedirs(info_path)
+
+    info_file = Path(os.path.join(info_path, 'info_{0}_sub{1}.gz'.format(dataset.code, str(subject).zfill(3))))
+    if os.path.exists(info_file):
+        with gzip.open(info_file, "r") as f:
+            info = pickle.load(f)
+    else:
+        X, y, metadata = paradigm.get_data(dataset=dataset, subjects=[subject], return_epochs=True)
+        info = {'sub': subject,
+                'dataset': dataset.code,
+                'ch_names': X.ch_names,
+                'classes': y,
+                'session': metadata.session,
+                'event_id': X.event_id
+                }
+        with gzip.open(info_file, "w") as f:  # save
+            pickle.dump(info, f)
+    return info
