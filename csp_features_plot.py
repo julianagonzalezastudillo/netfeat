a71003f6971c4c292200e54bb6024aa8f997576d
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2024-01-25 20:46:20 -0300

    include config functions and files

--- csp_features_plot.py
+++ csp_features_plot.py
@@ -1,42 +1,41 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-from pathlib import Path
-from moabb.datasets import (utils,
-                            BNCI2014001,
-                            Lee2019_MI,
-                            Zhou2016,
-                            MunichMI,
-                            Shin2017A,
-                            Cho2017,
-                            Schirrmeister2017,
-                            Weibo2014)
 import pickle
 import gzip
-from moabb.paradigms import LeftRightImagery
-from plot_tools import channel_pos_2D
+
 import matplotlib.colors
-import json
-import pathlib
+import matplotlib.pyplot as plt
+import numpy as np
+from moabb.datasets import (
+    BNCI2014001,
+    Lee2019_MI,
+    Zhou2016,
+    MunichMI,
+    Shin2017A,
+    Cho2017,
+    Schirrmeister2017,
+    Weibo2014,
+)
+from moabb.paradigms import LeftRightImagery
+from plottools.plot_positions import channel_pos_2d
+
+from config import load_config, ConfigPath
 
 
-path = pathlib.Path().resolve()
-jsonfile = "params.json"
-if jsonfile in os.listdir(path):
-    jsonfullpath = os.path.join(path, jsonfile)
-    with open(jsonfullpath) as jsonfile:
-        parameterDict = json.load(jsonfile)
-fmin = parameterDict['fmin']
-fmax = parameterDict['fmax']
-component_order = parameterDict['csp_component_order']
-ch_names = parameterDict['ch_names']
+# Load configuration parameters
+params = load_config()
+csp_component_order = params["csp_component_order"]
+ch_names = params["ch_names"]
 
-datasets = [BNCI2014001()]
-datasets = [BNCI2014001(), Cho2017(), Lee2019_MI(), Schirrmeister2017(), Weibo2014(), Zhou2016()]
+datasets = [
+    BNCI2014001(),
+    # Cho2017(),
+    # Lee2019_MI(),
+    # Schirrmeister2017(),
+    # Weibo2014(),
+    # Zhou2016(),
+]
 
-paradigm = LeftRightImagery(fmin=fmin, fmax=fmax)  # for rh vs lh
-mi_class = {'rh': 0,
-            'lh': 1}
+paradigm = LeftRightImagery(fmin=params["fmin"], fmax=params["fmax"])  # for rh vs lh
+mi_class = {"rh": 0, "lh": 1}
 # min-var component --> csp['patterns'][:, 0]
 # max-var component --> csp['patterns'][:, 1]
 
@@ -46,28 +45,33 @@ ch_dict = dict.fromkeys(ch_keys, [])
 ch_dict_sub = dict.fromkeys(ch_keys, [])
 
 # get electrodes positions
-ch_pos = channel_pos_2D(ch_keys)
+ch_pos = channel_pos_2d(ch_keys)
 
 # plot params
-colors = [[0.0, '#fffcf0'], [0.7, '#ffe17a'], [1.0, '#ffc600']]
+colors = [[0.0, "#fffcf0"], [0.7, "#ffe17a"], [1.0, "#ffc600"]]
 cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", colors)
 
-for csp_f_p in ['filters', 'patterns']:
-    for class_name in ['rh', 'lh']:
+for csp_f_p in ["filters", "patterns"]:
+    for class_name in ["rh", "lh"]:
         for dt in datasets:
+            # Loop through datasets
             for subject in dt.subject_list:
                 data = dt.get_data([subject])
                 sessions = [list(sessions.keys()) for sub, sessions in data.items()][0]
 
                 for session in sessions:
-                    file_name = Path(os.path.join(path, 'results', 'csp_features', 'csp_{0}_sub{1}_{2}_{3}.gz'
-                                                  .format(dt.code, str(subject).zfill(3), session, component_order)))
+                    file_name = (
+                        ConfigPath.RES_CSP
+                        / f"csp_{dt.code}_sub{str(subject).zfill(3)}_{session}_{csp_component_order}.gz"
+                    )
                     with gzip.open(file_name, "r") as f:
                         csp = pickle.load(f)
                         ch_names_dt = csp["info"].ch_names
 
                 # select first or second component according to which class
-                norm_csp = csp[csp_f_p][:, mi_class[class_name]]/max(abs(csp[csp_f_p][:, mi_class[class_name]]))
+                norm_csp = csp[csp_f_p][:, mi_class[class_name]] / max(
+                    abs(csp[csp_f_p][:, mi_class[class_name]])
+                )
                 for ch_i, idx in zip(ch_names_dt, np.arange(len(ch_names_dt))):
                     ch_dict_sub[ch_i] = np.append(ch_dict_sub[ch_i], norm_csp[idx])
                     # print('{0}: {1}'.format(ch_i, ch_dict_sub[ch_i]))
@@ -78,42 +82,59 @@ for csp_f_p in ['filters', 'patterns']:
 
         fig, ax = plt.subplots(figsize=(7, 5), dpi=300)
 
-        ch_dict = {k: v if not np.isnan(v) else 0 for k, v in ch_dict.items()}  # replace nan by 0
+        ch_dict = {
+            k: v if not np.isnan(v) else 0 for k, v in ch_dict.items()
+        }  # replace nan by 0
         ch_size = np.fromiter(ch_dict.values(), dtype=float)  # to array
         ch_name = ch_dict.keys()
 
         # plot csp values
         # fixed max size
-        ch_size_norm_to_plot = ch_size/max(abs(ch_size))
-        im = plt.scatter(ch_pos[:, 0], ch_pos[:, 1], s=(abs(ch_size_norm_to_plot)*fig.dpi/6)**2, c=ch_size, marker=".",
-                         cmap=cmap, alpha=0.9, linewidths=0, edgecolors='k')
+        ch_size_norm_to_plot = ch_size / max(abs(ch_size))
+        im = plt.scatter(
+            ch_pos[:, 0],
+            ch_pos[:, 1],
+            s=(abs(ch_size_norm_to_plot) * fig.dpi / 6) ** 2,
+            c=ch_size,
+            marker=".",
+            cmap=cmap,
+            alpha=0.9,
+            linewidths=0,
+            edgecolors="k",
+        )
 
         # add channel names
         for i, ch in zip(np.arange(len(ch_name)), ch_name):
-            plt.text(ch_pos[i, 0], ch_pos[i, 1], ch, fontsize=7, horizontalalignment='center',
-                     verticalalignment='center', c='k', fontname='Arial')
+            plt.text(
+                ch_pos[i, 0],
+                ch_pos[i, 1],
+                ch,
+                fontsize=7,
+                horizontalalignment="center",
+                verticalalignment="center",
+                c="k",
+                fontname="Arial",
+            )
 
         # plot handle
-        plt.gca().set_aspect('equal', adjustable='box')
-        plt.axis('off')
-        plt.title('{0} {1}'.format(csp_f_p, class_name))
+        plt.gca().set_aspect("equal", adjustable="box")
+        plt.axis("off")
+        plt.title("{0} {1}".format(csp_f_p, class_name))
 
-        # colorbar
-        cb_ax = fig.add_axes([.92, .124, .03, .7])
-        cbar = fig.colorbar(im, orientation='vertical', cax=cb_ax)
+        # Colorbar
+        cb_ax = fig.add_axes([0.92, 0.124, 0.03, 0.7])
+        cbar = fig.colorbar(im, orientation="vertical", cax=cb_ax)
         cbar.outline.set_linewidth(0.2)
-        cbar.outline.set_edgecolor('grey')
+        cbar.outline.set_edgecolor("grey")
         cbar.ax.tick_params(labelsize=7, size=0)
 
         plt.show()
-        fig_name = Path(os.path.join(os.getcwd(), 'results', 'csp_features', 'plot',
-                                     'csp_{0}_mean_across_sub_alpha_beta_{1}.png'.format(csp_f_p, class_name)))
+        fig_name = (
+            ConfigPath.RES_CSP
+            / f"plot/csp_{csp_f_p}_mean_across_sub_alpha_beta_{class_name}.png"
+        )
         fig.savefig(fig_name, transparent=True)
 
         # print 10 highest values channels
-        print('{0} {1}'.format(csp_f_p, class_name))
+        print(f"{csp_f_p} {class_name}")
         print(sorted(zip(list(ch_dict.values()), list(ch_name)), reverse=True)[:10])
-
-
-
-
--- plottools/plot_positions.py
+++ plottools/plot_positions.py
@@ -1,22 +1,12 @@
-import os
-import json
 import numpy as np
 from mne.viz.topomap import _get_pos_outlines
 import mne
 
 
-path = os.getcwd()
-jsonfile = "params.json"
-jsonfullpath = os.path.join(path, jsonfile)
-with open(jsonfullpath) as jsonfile:
-    parameterDict = json.load(jsonfile)
-PALETTE = parameterDict['palette']
-
-
-def channel_pos_2D(ch_names):
-    montage = mne.channels.make_standard_montage('standard_1005')
+def channel_pos_2d(ch_names):
+    montage = mne.channels.make_standard_montage("standard_1005")
     info = mne.create_info(montage.ch_names, sfreq=256, ch_types="eeg")
-    info.set_montage('standard_1005')  # to add digitization points
+    info.set_montage("standard_1005")  # to add digitization points
     picks = list(np.arange(0, len(montage.ch_names)))
     sphere = np.array([0, 0, 0, 0.95])
     pos, outlines = _get_pos_outlines(info, picks, sphere, to_sphere=True)
