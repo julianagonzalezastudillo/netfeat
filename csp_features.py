a503de34de9b151d986ab2f7fd97c0468a5afb6f
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2024-01-25 20:01:25 -0300

    add comments

--- csp_features.py
+++ csp_features.py
@@ -1,75 +1,94 @@
 """
-=================================
-  Save CSP filters and patterns
-=================================
+===========================================
+  NETFEAT - Save CSP filters and patterns
+===========================================
 This module is design to save CSP filters and patterns for each subject
 """
 # Authors: Juliana Gonzalez-Astudillo <julianagonzalezastudillo@gmail.com>
 #
-import numpy as np
-from mne.decoding import CSP
-from tools import suppress_stdout
-from moabb.datasets import (BNCI2014001,
-                            Cho2017,
-                            Lee2019_MI,
-                            MunichMI,
-                            Shin2017A,
-                            Schirrmeister2017,
-                            Weibo2014,
-                            Zhou2016)
-
-from moabb.paradigms import LeftRightImagery
 import copy as cp
-import os
-from pathlib import Path
 import pickle
 import gzip
-import json
-import pathlib
+
+import numpy as np
+
+from mne.decoding import CSP
+from moabb.paradigms import LeftRightImagery
+from tools import suppress_stdout
+from config import load_config, ConfigPath
+
+from moabb.datasets import (
+    BNCI2014001,
+    Cho2017,
+    Lee2019_MI,
+    MunichMI,
+    PhysionetMI,
+    Shin2017A,
+    Schirrmeister2017,
+    Weibo2014,
+    Zhou2016,
+)
 
 
-path = pathlib.Path().resolve()
-jsonfile = "params.json"
-if jsonfile in os.listdir(path):
-    jsonfullpath = os.path.join(path, jsonfile)
-    with open(jsonfullpath) as jsonfile:
-        parameterDict = json.load(jsonfile)
-fmin = parameterDict['fmin']
-fmax = parameterDict['fmax']
-n_components = parameterDict['csp_n_components']
-component_order = parameterDict['csp_component_order']
-
-# datasets = [BNCI2014001()]
-datasets = [Shin2017A(accept=True), Schirrmeister2017(), Weibo2014(), Zhou2016()]
-paradigm = LeftRightImagery(fmin=fmin, fmax=fmax)  # for rh vs lh
+# Load data
+datasets = [
+    BNCI2014001(),
+    Cho2017(),
+    Lee2019_MI(),
+    MunichMI(),
+    PhysionetMI(),
+    Shin2017A(accept=True),
+    Schirrmeister2017(),
+    Weibo2014(),
+    Zhou2016(),
+]
+params = load_config()
+paradigm = LeftRightImagery(fmin=params["fmin"], fmax=params["fmax"])  # for rh vs lh
+csp_component_order = params["csp_component_order"]
 
 for dt in datasets:
     for subject in dt.subject_list:
-        X, y, metadata = paradigm.get_data(dataset=dt, subjects=[subject], return_epochs=True)
+        # Get data for the current subject
+        X, y, metadata = paradigm.get_data(
+            dataset=dt, subjects=[subject], return_epochs=True
+        )
 
+        # Create a deep copy of the info object
         info = cp.deepcopy(X.info)
+
+        # Loop over sessions for the current subject
         for session in np.unique(metadata.session):
             ix = metadata.session == session
             X_session = X.get_data()[ix]
             y_session = y[ix]
 
-            csp = CSP(n_components=n_components, reg=None, transform_into='average_power', component_order=component_order)
+            # Create and fit CSP
+            csp = CSP(
+                n_components=params["csp_n_components"],
+                reg=None,
+                transform_into="average_power",
+                component_order=params["csp_component_order"],
+            )
             with suppress_stdout():
                 csp.fit_transform(X_session, y_session)
             patterns = csp.patterns_.T
             filters = csp.filters_.T
 
-            # save filters and patterns
-            csp_data = {'sub': subject,
-                        'code': dt.code,
-                        'info': X.info,
-                        'component_order': component_order,
-                        'session': metadata.session,
-                        'n_components': n_components,
-                        'filters': filters,
-                        'patterns': patterns}
-            csp_file = Path(os.path.join(os.getcwd(), 'results', 'csp_features', 'csp_{0}_sub{1}_{2}_{3}.gz'
-                                         .format(dt.code, str(subject).zfill(3), session, component_order)))
+            # Save filters and patterns
+            csp_data = {
+                "sub": subject,
+                "code": dt.code,
+                "info": X.info,
+                "component_order": params["csp_component_order"],
+                "session": metadata.session,
+                "n_components": params["csp_n_components"],
+                "filters": filters,
+                "patterns": patterns,
+            }
+            csp_file = (
+                ConfigPath.RES_CSP
+                / f"csp_{dt.code}_sub{str(subject).zfill(3)}_{session}_{csp_component_order}.gz"
+            )
             with gzip.open(csp_file, "w") as f:  # save
                 pickle.dump(csp_data, f)
 
