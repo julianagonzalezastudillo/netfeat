2c41b9bde51db629a78126e39dc367bc1863df9a
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2024-05-06 14:45:46 +0200

    add save_mat_file function to save .mat for 3D plot

--- tools.py
+++ tools.py
@@ -1,16 +1,19 @@
 from contextlib import contextmanager
 import sys, os
+import scipy.io as sio
+import numpy as np
+
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
 
 
 @contextmanager
@@ -22,3 +25,43 @@ def suppress_stdout():
             yield
         finally:
             sys.stdout = old_stdout
+
+
+def save_mat_file(ch_size, xyz, rgb, ch_name, metric, plot_path):
+    """Save .mat to do 3D brain plots.
+
+    Parameters
+    ----------
+    ch_size : {array-like} of shape (n_channels)
+        Vector with nodes values.
+
+    xyz : {array-like} of shape (n_channels)
+        3D nodes positions.
+
+    rgb : {array-like} of shape (n_channels, 4)
+        4D matrix with nodes colors.
+
+    ch_name : {array-like} of shape (n_channels)
+        Vector with nodes names.
+
+    metric : string
+        Metric name use to save .mat file.
+
+    plot_path : string
+        Path to save .mat file.
+
+    Returns
+    -------
+    save .mat
+    """
+
+    values = {
+        "Xnet": ch_size,
+        "xyz": xyz,
+        "color": rgb,
+        "names": np.array(ch_name),
+        "names_idx": np.arange(np.size(ch_name)),
+    }
+
+    sio.savemat(plot_path / f"{metric}.mat", values)
+    print(plot_path / f"{metric}.mat")
