27d565fb5ab293ee0aefc26aae18b4e2da5f1a17
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2024-05-08 16:08:47 +0200

    set new eeg positions for 2d layout

--- src/plottools/plot_positions.py
+++ src/plottools/plot_positions.py
@@ -1,9 +1,15 @@
+import json
 import numpy as np
 from mne.viz.topomap import _get_pos_outlines
+from mne.channels import make_dig_montage
 import mne
 
 
-def channel_pos(ch_names, dimension="2d"):
+def channel_pos(
+    ch_names,
+    dimension="2d",
+    montage_type="file",
+):
     """Get the 2D or 3D positions of EEG channels.
 
     Parameters
@@ -13,17 +19,37 @@ def channel_pos(ch_names, dimension="2d"):
     dimension : {'2d', '3d'}, optional
         Dimension of the positions to retrieve, by default '2d'.
 
+    montage_type : {'file', 'standard'}, optional
+        Type of montage to use with different eeg positions, by default 'file'.
+
     Returns
     -------
     ch_pos : np.ndarray
         Array containing the positions of EEG channels.
     """
-    # Load standard montage
-    montage = mne.channels.make_standard_montage("standard_1005")
+    # Create info structure using the selected montage
+    if montage_type == "file":
+        # Load montage from file
+        with open("eeg_positions.json", "r") as f:
+            eeg_positions = json.load(f)
 
-    # Create info structure
-    info = mne.create_info(montage.ch_names, sfreq=256, ch_types="eeg")
-    info.set_montage("standard_1005")  # Add digitization points
+        montage = make_dig_montage(
+            ch_pos=eeg_positions["ch_pos"],
+            nasion=eeg_positions["nasion"],
+            lpa=eeg_positions["lpa"],
+            rpa=eeg_positions["rpa"],
+            hsp=eeg_positions["hsp"],
+            hpi=eeg_positions["hpi"],
+        )
+        info = mne.create_info(montage.ch_names, sfreq=256, ch_types="eeg")
+        with info._unlock():
+            info["dig"] = montage.dig
+
+    elif montage_type == "standard":
+        # Load standard montage
+        montage = mne.channels.make_standard_montage("standard_1005")
+        info = mne.create_info(montage.ch_names, sfreq=256, ch_types="eeg")
+        info.set_montage("standard_1005")  # Add digitization points
 
     # Get positions and outlines
     picks = list(range(len(montage.ch_names)))
