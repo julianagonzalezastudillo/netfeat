8665f4e38a8eb3492ef7a191545b4eed54b41d64
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2024-05-23 14:33:54 +0200

    add psd and order pipelines

--- src/plot_accuracies.py
+++ src/plot_accuracies.py
@@ -6,55 +6,46 @@ Plot classification results. Here we plot the results using some of the convenie
 The score_plot visualizes all the data with one score per subject for every dataset and pipeline.
 """
 import pandas as pd
-from moabb.datasets import (
-    BNCI2014001,
-    Cho2017,
-    Lee2019_MI,
-    MunichMI,
-    PhysionetMI,
-    Shin2017A,
-    Schirrmeister2017,
-    Weibo2014,
-    Zhou2016,
-)
 from moabb.analysis.meta_analysis import compute_dataset_statistics
 import matplotlib.pyplot as plt
 from plottools.plot_scores import score_plot, meta_analysis_plot
-from config import ConfigPath, load_config
+from config import ConfigPath, load_config, DATASETS
 
 
 # Define constants
-PIPELINES = ["net", "csp", "riemannian"]
+PIPELINES = ["net", "csp", "riemannian", "psd_"]
 params, paradigm = load_config()
-
-# Load datasets
-datasets = [
-    BNCI2014001(),
-    Cho2017(),
-    Lee2019_MI(),
-    MunichMI(),
-    PhysionetMI(),
-    Shin2017A(accept=True),
-    Schirrmeister2017(),
-    Weibo2014(),
-    Zhou2016(),
-]
 
 # Load results
 results = pd.DataFrame()
-for dt in datasets:
+for dt in DATASETS:
     for pipeline in PIPELINES:
         filename = ConfigPath.RES_CLASSIFY_DIR / f"{dt.code}_rh_lh_{pipeline}.csv"
         df_dt = pd.read_csv(filename)
         results = pd.concat([results, df_dt], ignore_index=True)
 
 # Filter and consolidate results
-select_pipelines = ["RG+SVM", "CSP+PS+SVM"]
+select_pipelines = ["PSD+SVM", "CSP+PS+SVM", "RG+SVM"]
 select_results = results[results["pipeline"].isin(select_pipelines)]
 
 for name, metric in params["net_metrics"].items():
     select = results[results["pipeline"] == f"coh+{name}+SVM"]
     select_results = pd.concat([select_results, select], ignore_index=True)
+
+# Sort by custom order
+pipeline_order = [
+    "PSD+SVM",
+    "CSP+PS+SVM",
+    "RG+SVM",
+    "coh+s+SVM",
+    "coh+lat+SVM",
+    "coh+seg+SVM",
+    "coh+intg+SVM",
+]
+select_results["pipeline"] = pd.Categorical(
+    select_results["pipeline"], categories=pipeline_order, ordered=True
+)
+select_results = select_results.sort_values("pipeline")
 
 # Plot accuracies
 fig, ax = score_plot(select_results)
@@ -63,8 +54,8 @@ plt.show()
 # Plot stats
 stats = compute_dataset_statistics(results)
 for algo, algo_name in zip(
-    ["RG+SVM", "coh+intg+SVM", "coh+lat+SVM", "coh+s+SVM", "coh+seg+SVM"],
-    ["RG+SVM", "intg+SVM", "lat+SVM", "s+SVM", "seg+SVM"],
+    ["RG+SVM", "coh+s+SVM", "coh+lat+SVM", "coh+seg+SVM", "coh+intg+SVM"],
+    ["RG+SVM", "s+SVM", "lat+SVM", "seg+SVM", "intg+SVM"],
 ):
     fig, ax = meta_analysis_plot(stats, algo, "CSP+PS+SVM", algo_name, "CSP+SVM")
     plt.show()
