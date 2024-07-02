cda91893d626ec061a221c3fad5e82c11f766116
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2024-07-02 11:36:53 +0200

    add comments to processing pipeline

--- src/netfeat_pipeline.py
+++ src/netfeat_pipeline.py
@@ -1,8 +1,11 @@
 """
 ==========================================
- Network Metrics & Functional Connectivity
+ NETFEAT - Processing pipeline
 ==========================================
-This module is design to compute network metrics based on functional connectivity for MOABB datasets
+
+This script implements the NETFEAT processing pipeline, which includes
+data loading, feature extraction, and evaluation using cross-validation.
+Designed to work with functional connectivity & network properties.
 """
 
 import numpy as np
@@ -31,11 +34,17 @@ from tqdm.auto import tqdm
 from config import ConfigPath
 
 
+# Initialize logging
 log = logging.getLogger(__name__)
 save_features.init()
 
 
-class WithinSessionEvaluation_netfeat(BaseEvaluation):
+class WithinSessionEvaluationNetfeat(BaseEvaluation):
+    """
+    A class for evaluating within-session performance of various pipelines including functional connectivity & network
+    properties. Inherits from BaseEvaluation in MOABB.
+    """
+
     def is_valid(self, dataset):
         return True
 
@@ -57,13 +66,17 @@ class WithinSessionEvaluation_netfeat(BaseEvaluation):
             if len(run_pipes) == 0:
                 continue
 
+            # Load data for the current subject
             X, y, metadata = self.paradigm.get_data(
                 dataset=dataset, subjects=[subject], return_epochs=True
             )
+
             for name, clf_preproc in pipelines.items():
                 t_start = time()
                 cv = StratifiedKFold(5, shuffle=False, random_state=None)
                 dataset.sub = str(subject)
+
+                # Add attributes to each step in the pipeline
                 [
                     add_attributes(
                         clf_preproc[clf_preproc.steps[i][0]],
@@ -78,14 +91,17 @@ class WithinSessionEvaluation_netfeat(BaseEvaluation):
                     for i in range(len(clf_preproc.steps))
                 ]
 
+                # Optimize functional connectivity and network metrics
                 X_, clf = self.fc_net(X, clf_preproc)
 
                 for session in np.unique(metadata.session):
                     ix = metadata.session == session
                     X_session = np.array(X_[ix])
-                    le = LabelEncoder()  # to change string labels to numerical ones
+                    le = LabelEncoder()
                     y_session = le.fit_transform(y[ix])
                     nchan = np.array(X_).shape[1]
+
+                    # Add session attributes to each step in the pipeline
                     [
                         add_attributes(clf[clf.steps[i][0]], session=session)
                         for i in range(len(clf.steps))
@@ -130,6 +146,7 @@ class WithinSessionEvaluation_netfeat(BaseEvaluation):
         ----------
         pipelines : dict of pipeline instance.
             A dict containing the sklearn pipeline to evaluate.
+
         param_grid : dict of str
             The key of the dictionary must be the same as the associated pipeline.
 
@@ -156,14 +173,29 @@ class WithinSessionEvaluation_netfeat(BaseEvaluation):
 
         return self.results.to_dataframe(pipelines=pipelines)
 
-    def fc_net(self, X, clf_preproc):
+    @staticmethod
+    def fc_net(X, clf_preproc):
+        """For optimisation functional connectivity and network metrics are computed and saved outside the
+        cross-validation.
+        If the files contained this data already exists, they are loaded.
+
+        Parameters
+        ----------
+        X : mne.Epochs
+            The EEG data.
+
+        clf_preproc : Pipeline
+                The preprocessing pipeline.
+
+        Returns
+        -------
+        X : ndarray
+            The transformed data.
+
+        clf : Pipeline
+            The modified pipeline.
         """
-        For optimisation functional connectivity and network metrics are computed and saved outside the
-        cross-validation. If the files contained this data already exists, they are loaded.
-        :return:
-        X: nd.array
-        clf: pipeline
-        """
+
         clf = deepcopy(clf_preproc)
         content = [clf.steps[i][0] for i in range(len(clf.steps))]
 
@@ -177,9 +209,11 @@ class WithinSessionEvaluation_netfeat(BaseEvaluation):
                 subject = clf.named_steps[estimator].subject
                 dataset = clf.named_steps[estimator].dataset
 
+                # Define the filename for saving/loading the estimator data
                 file_name = f"{estimator}_{dataset.code}_{str(subject).zfill(3)}_{'-'.join(list(X.event_id))}_{method}.gz"
                 file_path = ConfigPath.RES_DIR / estimator
                 file = file_path / file_name
+
                 if os.path.exists(file):
                     # Load data
                     with gzip.open(file, "r") as f:
@@ -189,19 +223,22 @@ class WithinSessionEvaluation_netfeat(BaseEvaluation):
                     # Compute estimator
                     preproc = clone(clf.named_steps[estimator])
                     X._data = preproc.transform(X=X)
-                    # Save
+
+                    # Save the computed data
                     file_path.mkdir(parents=True, exist_ok=True)
                     with gzip.open(file, "w") as f:  # save
                         data = {method: X._data}
                         pickle.dump(data, f)
+
+                # Remove the estimator from the pipeline
                 step_index = [
                     i for i, (name, _) in enumerate(clf.steps) if name == estimator
                 ][0]
                 clf.steps.pop(step_index)
+
+                # Add metric attributes to remaining steps
                 [
                     add_attributes(clf[clf.steps[i][0]], metric=method)
                     for i in range(len(clf.steps))
                 ]
-            # else:
-            # concatenate connectivity matrix
         return X._data, clf
