c93fa8d99806fc3c7ee924b02e7946c920af28d8
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2024-07-02 17:21:07 +0200

    update comments for PSD computation

--- src/psdtools/psd.py
+++ src/psdtools/psd.py
@@ -1,9 +1,33 @@
+"""Estimation of power spectrum density."""
 from sklearn.base import BaseEstimator, TransformerMixin
 from mne.time_frequency import psd_array_welch
 import numpy as np
 
 
 class PSDWelch(BaseEstimator, TransformerMixin):
+    """
+    Compute power spectrum density using Welch's method.
+
+    Parameters:
+    -----------
+    fmin : float, optional (default=8)
+        The minimum frequency of the band of interest.
+
+    fmax : float, optional (default=35)
+        The maximum frequency of the band of interest.
+
+    sfreq : float
+        The sampling frequency.
+
+    n_overlap : int
+        The number of points of overlap between segments. Will be adjusted
+        to be <= n_per_seg. The default value is 0.
+
+    n_per_seg : int | None
+        Length of each Welch segment (windowed with a Hamming window). Defaults
+        to None, which sets n_per_seg equal to n_fft.
+    """
+
     def __init__(self, fmin=8, fmax=35, n_overlap=0, sfreq=None, n_per_seg=None):
         self.fmin = fmin
         self.fmax = fmax
@@ -18,7 +42,7 @@ class PSDWelch(BaseEstimator, TransformerMixin):
     def transform(self, X, y=None):
         # Apply the PSD transformation
         psd, freqs = psd_array_welch(
-            X,
+            X._data,
             sfreq=self.sfreq,
             fmin=self.fmin,
             fmax=self.fmax,
